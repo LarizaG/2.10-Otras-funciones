f5a912cd520020206c8e45af7958f9670204ca8c
JENNI-ALMARAZ <91512122+JENNI-ALMARAZ@users.noreply.github.com>
2021-11-25 21:09:22 -0600

    Add files via upload

---
+++ Fafin.m
@@ -0,0 +1,17 @@
+% Octave Script
+% Title         :Funcion Afin 
+% Author        :Jennifer Almaraz Suarez
+% Description   :Script para graficar una funcion
+% Date          :20211026
+% Version       :1
+% Usage         :octave>cd /path/
+%               :octave>Fafin
+%               :Requiere aplicacion de octave, usar su linea de comandos
+
+clear
+% Dominio de la funcion
+x=-10:1:10;
+% Rango de la funcion
+fx=(2*x-2);
+% Funcion a plotear
+plot(x, fx); 
---
+++ Fident.m
@@ -0,0 +1,18 @@
+% Octave Script
+% Title         :Funcion Afin
+% Author        :Jennifer Almaraz Suarez
+% Description   :Script para graficar una funcion
+% Date          :20211026
+% Version       :1
+% Usage         :octave>cd /path/
+%               :octave>Fafin
+%               :Requiere aplicacion de octave, usar su linea de comandos
+
+clear
+% Dominio de la funcion
+x=-10:1:10;
+% Rango de la funcion
+fx=(5*x);
+% Funcion a plotear
+plot(x, fx);
+
