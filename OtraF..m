2b84595c7ad804bf3b949e2bd999504c12103d74
PatriciaZP <91707472+PatriciaZP@users.noreply.github.com>
2021-11-25 19:46:47 -0600

    Add files via upload

---
+++ OtraF..m
@@ -0,0 +1,20 @@
+% Ocatve Script
+% Title                :Otro tipo de funciones 
+% Description          :Script para identificar otro tipo de funciones 
+% Author               :Patricia Zaragoza Palma
+% Date                 :20211125
+% Version              :1
+% Usege                :octave>cd /path/
+%                      :octave>Otrotipodefuncion
+%                      :Requiere aplicacion octave, usar su linea de comandos 
+% Notes                :usar comandos 
+
+
+
+clear
+% Dominio de la Funcion 
+x= -50:1:50
+% Reglas de correspondencia (Funcion)
+y= 2.*x+1
+% Plotear funcion 
+plot(x, y);
